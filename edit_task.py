bc448925c6ace6157c99858aff2dd96a40a0b171
ConquerorCletus <smartchibuike2019@gmail.com>
2023-09-03 19:12:59 +0000

    edit task documentation and file handling mode

--- edit_task.py
+++ edit_task.py
@@ -7,10 +7,11 @@ import datetime
 import show
 
 def edit_task():
+    """This function edit the tasks"""
     show.show()
     selected_task = show.select()
 
-    with open(selected_task, 'r') as editted_task:
+    with open(selected_task, 'r+') as editted_task:
         task_content = editted_task.read()
         print(task_content)
 
