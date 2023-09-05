5aac361dca6d138dcbb863f7e1d6311597586fb7
ConquerorCletus <smartchibuike2019@gmail.com>
2023-09-05 16:56:59 +0000

    figuring out new add task, still encountering some errors but in the right direction

--- add_task.py
+++ add_task.py
@@ -7,23 +7,29 @@ import datetime
 import os
 import json
 
-def collect_data():
+def add_task():
+    """This functions add a task to the To-do List"""
+
+    # collect input data to store in a dictionary called task
     task_name = input("Enter Task name: ")
     task_desc = input("Enter task Description: ")
     task_created_at = datetime.datetime.now()
     remind_at = input ("Please enter a time to perform task in HH:MM (am/pm)format: ")
+
+    # To Ensure user entered the right time format
     try:
         remind_at = datetime.datetime.strptime(remind_at, '%HH:%MM')
     except ValueError:
         print("Invalid date/time format, use the format HH:MM")
-        return None
 
+    # The dictionary created to store inputs
     tasks = {
             'task title:': task_name,
             'task Description:': task_desc,
             'Time logged:': task_created_at,
-            'Deadline:': remind_at.strftime('%HH:%MM')
+            'Deadline:': remind_at
             }
+
     # file handling section
     directory_path = './task'
     file_name = "{}.json".format(tasks['task title:'])
@@ -33,14 +39,9 @@ def collect_data():
     if not os.path.exists(directory_path):
         os.makedirs(directory_path)
 
-    # return(file_path)
-    # return (tasks)
-
-def add_task():
-    # file_path = collect_data()
     try:
         with open(file_path, 'w') as file:
-
+            json.dumps(tasks, file, indent=4)
             # file.write(f'Task: {task_name} \n')
             # file.write(f'Description: {task_desc}\n')
             # file.write(f'Time logged: {task_created_at} \n')
@@ -48,7 +49,7 @@ def add_task():
             print("Task successfully added!!!")
 
     except Exception as e:
-       print(f"You don't have permission to create a file in {e}.")
+        print(f"You don't have permission to create a file in {e}.")
 
 
 add_task()
