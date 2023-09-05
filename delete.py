729dd4e378edb4ac7dca55e03262d1c8be99c22e
ConquerorCletus <smartchibuike2019@gmail.com>
2023-09-05 07:03:48 +0000

    delete function

---
+++ delete.py
@@ -0,0 +1,26 @@
+"""This module contains a functions that delete tasks"""
+
+import show
+import os
+
+def delete_task():
+    """This function delete tasks from tasks"""
+    show.show()
+    print("Select the task to delete !!")
+    task = show.select()
+
+    if task == task:
+        option = input("Are you sure you want to delete task? yes/no : ")
+        if option == 'yes':
+            os.remove(task)
+            print("Task deleted !!")
+        elif option == 'no':
+            print("Have a nice day!!")
+        else:
+            print("Invalid Option !!")
+
+    else:
+        print("No task selected to delete")
+
+
+delete_task()
