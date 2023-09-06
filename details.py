8ac41217d67860f1d7b43bf5e6bcfa497acbb12b
ConquerorCletus <smartchibuike2019@gmail.com>
2023-09-06 17:40:39 +0000

    details with exception and updated edit task

--- details.py
+++ details.py
@@ -7,15 +7,18 @@ import os
 def show_details(show_task):
     """This shows the details of the tasks"""
     # show_task = show.select()
-    if show_task == None:
-        print("No task available!!")
-    else:
+    # if show_task == None:
+    #     print("No task available!!")
+    # else:
+    try:
         with open(show_task, 'r') as task:
             tasks = json.load(task)
-
+        
         for key, value in tasks.items():
             print(f"{key} : {value}")
 
-    
+    except Exception as e:
+        print(f"You encountered this {e}")
+
 
 # show_details()
--- edit_task.py
+++ edit_task.py
@@ -9,8 +9,8 @@ import show
 import json
 
 def edit_task():
-    """This function edit the tasks"""
     show.show()
+    """This function edit the tsaks"""
     selected_task = show.select()
     time_updated = datetime.datetime.now()
     # to open the selected task for editting
@@ -18,7 +18,7 @@ def edit_task():
     
     print('''
          what do you want to edit?\n
-         1. Task name
+         1. Task title
          2. Task Description
          3. Deadline
          4. quit \n
@@ -48,39 +48,11 @@ def edit_task():
 
         details = input("Do you want to see new details of task? yes/no: ")
         if details == 'yes':
-            show_details(editted_task)
+            show_details(selected_task)
         elif details == 'no':
             print("Exiting program!!!")
               
-      # # show details of tasks with a specific line number
-      #   for line, details in enumerate(task_line):
-      #       print(f"{line + 1}: {details}")
-            
-
-        # edit_line = int(input("The task line to edit: ")) - 1
-        # new_content = input("New Task content: ")
-       
-       # # This ensures that user input the correct task line
-       #  if edit_line <= len(task_line):
-       #      task_line[edit_line] = new_content + '\n'
-       #      editted_task.seek(0)
-       #      editted_task.writelines(task_line)
-       #      editted_task.truncate()
-       #      print("Task editted successfully!!")
-       #  else:
-       #      print("Invalid task line selected!!")
-
-       #  show_option = input("Do you want to see details of task? yes/no: ")
-       #  # show_option = show_option.lower
-
-       #  if show_option == 'yes':
-       #      show_details()
-       #  elif show_option == 'no':
-       #      print("Exiting!! Exiting!!")
-       #  else:
-       #      print("Invalid choice!! ")
-
-
+         
 
 
 edit_task()
