9b02950a8c5e5bbcd01cc011a239325fe8907b55
ConquerorCletus <smartchibuike2019@gmail.com>
2023-09-13 00:17:25 +0100

    Interface of Task manager completed, main.py

--- main.py
+++ main.py
@@ -4,38 +4,51 @@ import edit_task
 import details
 import delete
 import reminder
+import os
 
-
+def clear_screen():
+    os.system("cls" if os.name == "nt" else "clear")
+    
 
 
 def main():
-    print('''
+    while True:
+        clear_screen()
+        print('''
 
-          \t\t________WELCOME TO TASK MANAGER_________
-          
-          \t\tWE help you manage your day
+              \t\t\t\t________WELCOME TO TASK MANAGER_________
 
-          \t\tWhat would you like to do:
+              \t\t\t\tWE help you manage your day
 
-          \t\t(1) ADD A TASK
-          \t\t(2) EDIT A TASK
-          \t\t(3) SHOW YOUR TASKS
-          \t\t(4) DELETE A TASK
-          \t\t(5) QUIT PROGRAM
-    ''')
-    option = int(input("Enter your Option: "))
-    if option == 1:
-        add_task.add_task()
-    elif option == 2:
-        edit_task.edit_task()
-    elif option == 3:
-        show.show()
-    elif option == 4:
-        delete.delete_task()
-    elif option == 5:
-        print("EXITING PROGRAM!!!")
-    else:
-        print("INVALID OPTION. ENTER BETWEEN OPTION (1-5)")
+              \t\t\t\tWhat would you like to do:
+
+              \t\t\t\t(1) ADD A TASK
+              \t\t\t\t(2) EDIT A TASK
+              \t\t\t\t(3) SHOW YOUR TASKS
+              \t\t\t\t(4) DELETE A TASK
+              \t\t\t\t(5) QUIT PROGRAM
+              
+              ''')
+        option = int(input("Enter your Option: "))
+        if option == 1:
+            add_task.add_task()
+        elif option == 2:
+            edit_task.edit_task()
+        elif option == 3:
+            show.show()
+        elif option == 4:
+            delete.delete_task()
+        elif option == 5:
+            print("EXITING PROGRAM!!!")
+            break
+        else:
+            print("INVALID OPTION. ENTER BETWEEN OPTION (1-5)")
+
+        choice = input("Do you want to continue? Yes/no : ")
+        if choice.lower != 'yes':
+            print("GOODBYE !! EXITING PROGRAM !!")
+            break
+
 
 
 main()
